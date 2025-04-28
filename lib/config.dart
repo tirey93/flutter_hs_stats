f6b395b7af657577d8b95e782215b5db9918c081
tirey93 <tirey93@gmail.com>
2025-04-28 10:06:41 +0200

    update after emerald

--- lib/config.dart
+++ lib/config.dart
@@ -1,19 +1,24 @@
 import 'package:hs_stats/data/expansion.dart';
 
 class Config {
-  static String get lastRotationDate => "2024-03-19";
+  static String get lastRotationDate => "2025-03-25";
   static Map<String, Expansion> get expansions => {
+    "EMERALD_DREAM": Expansion("Emerald Dream", "Year of the Raptor", "Emerald", 2025, 3),
+    "_EXP2": Expansion("???", "Year of the Raptor", "???", 2025, 7),
+    "_EXP3": Expansion("???", "Year of the Raptor", "???", 2025, 11),
+
     "WHIZBANGS_WORKSHOP": Expansion("Whizbang's Workshop", "Year of the Pegasus", "Whizbang", 2024, 3),
     "ISLAND_VACATION": Expansion("Perils in Paradise", "Year of the Pegasus", "Perils", 2024, 7),
     "SPACE": Expansion("Great Dark Beyond", "Year of the Pegasus", "Beyond", 2024, 11),
 
-    "BATTLE_OF_THE_BANDS": Expansion("Festival of Legends", "Year of the Wolf", "Festival", 2023, 4),
-    "TITANS": Expansion("Titans", 'Year of the Wolf', "Titans", 2023, 8),
-    "WILD_WEST": Expansion("Showdown in the Badlands", "Year of the Wolf", "Badlands", 2023, 11),
-
     "WILD": Expansion("Wild", 'Wild', "Wild", null, null),
   };
   static Set<String> get uncollectibleSignatures => {
+    //EMERALD
+    "Ancient of Yore",
+    "Hopeful Dryad",
+    "Tranquil Treant"
+
     //SPACE
     "Red Giant",
     "Galactic Crusader",
