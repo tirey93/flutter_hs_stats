34186c5ed56932a8b90608b51f073c6271855a7c
tirey93 <tirey93@gmail.com>
2026-05-04 11:55:28 +0200

    added simple bat

---
+++ sources/hs_update.bat
@@ -0,0 +1,13 @@
+@echo off
+echo ========================================
+echo Hearthstone Stats Updater
+echo ========================================
+echo.
+
+C:\Users\tirey93\.conda\envs\python38\python.exe hs_update.py
+
+echo.
+echo ========================================
+echo Press any key to close...
+echo ========================================
+pause > nul
